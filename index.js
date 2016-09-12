f06d4c80b8ef50cc4f1c1c9599f42b075c37404b
Florian Golemo <Florian Golemo>
2016-09-12 04:29:44 +0200

    fiddled with the packages

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ var serveStatic = require('serve-static');
 var serve = serveStatic('public/', {'index': ['index.html']});
 
 // Create server
-var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, finalhandler(req, res))
+var server = http.createServer(function onRequest(req, res) {
+    serve(req, res, finalhandler(req, res))
 });
 
 // Listen
 server.listen(process.env.PORT || 5000);
 
+//console.log('Server running at http://127.0.0.1:8125/');
